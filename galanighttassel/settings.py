9670ebe17ce80ffe7f32fee62066e42ff7b6ea9f
koheisanno <kohei.sanno@gmail.com>
2019-10-05 15:57:36 +0800

    seetingschange

--- galanighttassel/settings.py
+++ galanighttassel/settings.py
@@ -124,12 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+DISABLE_COLLECTSTATIC=1
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
