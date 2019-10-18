0fbf33160f1bfa5721aeb6d1f67087b1afe071ba
koheisanno <kohei.sanno@gmail.com>
2019-10-18 12:26:18 +0800

    s3

--- galanighttassel/settings.py
+++ galanighttassel/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-#DISABLE_COLLECTSTATIC=1
+DISABLE_COLLECTSTATIC=1
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
