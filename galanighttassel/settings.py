439962fdebc3b2cb531097ddaa156d95ac2f53ec
koheisanno <kohei.sanno@gmail.com>
2019-10-18 19:45:38 +0800

    rollback

--- galanighttassel/settings.py
+++ galanighttassel/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
-ALLOWED_HOSTS = ['tasselgala2019.herokuapp.com', '127.0.0.1',]
+ALLOWED_HOSTS = ['tasselgala2019.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paypal.standard.ipn',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -143,14 +142,5 @@ MEDIA_URL = '/media/'
 
 PAYPAL_RECEIVER_EMAIL='testingdjango@business.com'
 PAYPAL_TEST= True
-
-AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_SECRET_ACCESS_KEY')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL= None
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
