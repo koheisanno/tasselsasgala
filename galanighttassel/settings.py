7c50faaf53a45484a4a0d4a1f7d2edf500ba937e
koheisanno <kohei.sanno@gmail.com>
2019-10-18 18:22:22 +0800

    s3

--- galanighttassel/settings.py
+++ galanighttassel/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'paypal.standard.ipn',
-    #'storages',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -144,13 +144,13 @@ MEDIA_URL = '/media/'
 PAYPAL_RECEIVER_EMAIL='testingdjango@business.com'
 PAYPAL_TEST= True
 
-#AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
-#AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
-#AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-#AWS_S3_FILE_OVERWRITE = False
-#AWS_DEFAULT_ACL= None
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL= None
 
-#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
