241a4e041cf1597897815e19af499a88371fcda7
koheisanno <kohei.sanno@gmail.com>
2019-10-29 22:48:22 +0800

    donate

--- donate/donateform.py
+++ donate/donateform.py
@@ -4,4 +4,4 @@ from donate.models import Donation, DonationCount
 class Donate(forms.ModelForm):
     class Meta:
         model=Donation
-        fields=['donation_type','number_of_items','your_donation']
+        fields=['donation_type','number_of_items','your_donation','first_Name','last_Name','email']
